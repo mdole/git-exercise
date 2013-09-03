e6c0b4239df466566cc959e32933cdc2e6b68e48
dolematt <dolematt@grinnell.edu>
2013-09-03 10:47:39 -0500

    Simple print

---
+++ src/edu/grinnell/csc207/username/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.grinnell.csc207.username.hello;
+
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+        System.out.println("Hello, GitHub!");
+    }
+
+}
